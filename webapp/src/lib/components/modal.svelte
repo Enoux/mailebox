b865577c8a46bb63d28951ec6b6a35134fbba7a1
Benjeouelle Lazaro <balazaro1@up.edu.ph>
2026-04-26 23:29:51 +0800

    feat: add customizable modal component

---
+++ webapp/src/lib/components/modal.svelte
@@ -0,0 +1,18 @@
+<script>
+    import x from '$lib/assets/icons/x.svg';
+
+    let { modal_content, active = $bindable() } = $props();
+</script>
+
+<div id="otp_modal" class="bg-black/50 z-100 w-screen h-screen absolute top-0 left-0 flex">
+    <div class="flex flex-col w-1/2 bg-mlb-white rounded-xl m-auto p-4">
+        <!-- Close Button -->
+        <button class="ml-auto" onclick={() => active = false}>
+            <img src={x} alt="Close Modal" >
+        </button>
+
+        <div class="w-full m-2">
+            {@render modal_content()}
+        </div>
+    </div>
+</div>
