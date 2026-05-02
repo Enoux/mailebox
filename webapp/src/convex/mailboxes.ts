f42c1c24421a3234fe03c2eb5049c4817b8c9a49
Benjeouelle Lazaro <balazaro1@up.edu.ph>
2026-05-02 19:28:25 +0800

    feat: update mailbox query to merge with parcels (if applicable)

--- webapp/src/convex/mailboxes.ts
+++ webapp/src/convex/mailboxes.ts
@@ -4,10 +4,16 @@ import { query } from "./_generated/server.js";
 export const getMailboxes = query({
     args: {},
     handler: async (ctx) => {
-        // Get most recent messages first
         const mailboxes = await ctx.db.query("mailboxes").collect();
-        // Reverse the list so that it's in a chronological order.
-        console.log(mailboxes)
-        return mailboxes;
+
+        const parcels = await ctx.db.query("parcels").collect();
+
+        const merge = mailboxes.map((mailbox) => ({
+            ...mailbox,
+            parcel_info: parcels.find((p) => p._id === mailbox.parcel_id)
+        }));
+
+        console.log(merge);
+        return merge;
     },
 });
