8b0813fe2a454b2fa391b25afb58bb6b2d5a80bb
kalatkarin <carlynecconsebido@gmail.com>
2026-05-09 16:52:22 +0800

    feat: added explicit button in table row and enabled/disabled based on parameters

--- webapp/src/lib/components/table_row_courier.svelte
+++ webapp/src/lib/components/table_row_courier.svelte
@@ -19,26 +19,29 @@
 	}
 </script>
 
-<button
-	class="bg-mlb-white border-mlb-white hover:border-mlb-orange mb-2 flex h-1/12 w-full shrink-0 flex-row rounded-2xl border-2 hover:shadow-sm"
-	onclick={handleClick}
+<div
+	class="bg-mlb-white border-mlb-white hover:border-mlb-orange mb-2 flex h-1/10 w-full shrink-0 flex-row rounded-2xl border-2 hover:shadow-sm"
 >
-	<div class="w-1/4 content-center text-center font-bold">
+	<div class="w-1/5 content-center text-center font-bold">
 		{parcel_num}
 	</div>
 
-	<div class="w-1/4 content-center text-center">
+	<div class="w-1/5 content-center text-center">
 		{locker_num}
 	</div>
 
-	<div class="w-1/4 content-center text-center">
+	<div class="w-1/5 content-center text-center">
 		{recipient_uid}
 	</div>
 
-	<div class="w-1/4 content-center text-center">
+	<div class="w-1/5 content-center text-center">
 		{status}
 	</div>
-</button>
+
+	<div class="w-1/5 content-center text-center">
+		<button class="bg-mlb-orange text-mlb-white px-3 py-1 rounded-lg font-bold drop-shadow-sm enabled:hover:brightness-90 disabled:bg-mlb-gray" onclick={handleClick} disabled={status != "Sorting"}> Deliver </button>
+	</div>
+</div>
 
 <!-- Locker Override Modal -->
 {#snippet modal_content()}
