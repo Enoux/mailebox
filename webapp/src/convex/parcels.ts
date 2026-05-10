5714163ec27e85b998b9f3c94355ab40fe5079ca
Benjeouelle Lazaro <balazaro1@up.edu.ph>
2026-05-10 19:49:12 +0800

    feat(parcels): add convex fxn for obtaining parcel using its tracking id

--- webapp/src/convex/parcels.ts
+++ webapp/src/convex/parcels.ts
@@ -1,3 +1,4 @@
+import { v } from 'convex/values';
 import { query } from './_generated/server.js';
 
 export const getParcels = query({
@@ -6,5 +7,24 @@ export const getParcels = query({
 		const parcels = await ctx.db.query('parcels').collect();
 		console.log(parcels);
 		return parcels;
+	}
+});
+
+export const getParcel = query({
+	args: { tracking_num: v.string() },
+	handler: async (ctx, args) => {
+		const parcels = await ctx.db.query('parcels').collect();
+		const mailboxes = await ctx.db.query('mailboxes').collect();
+
+		const userParcel = parcels.find((p) => p.tracking_id === args.tracking_num);
+
+		if (userParcel == undefined) return userParcel;
+
+		const mailbox = mailboxes.find((m) => m.parcel_id === userParcel._id);
+
+		return {
+			parcel_info: userParcel,
+			locker_num: mailbox ? mailbox.locker_number : 'N/A'
+		};
 	}
 });
