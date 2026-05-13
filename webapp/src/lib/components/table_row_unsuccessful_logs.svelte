23aaa8bcc4bad022e5675002420dd99a45f602d0
Carlyne Consebido <karin@Carlynes-MacBook.local>
2026-05-13 16:33:31 +0800

    feat: added bare bones dashboard layout

---
+++ webapp/src/lib/components/table_row_unsuccessful_logs.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+
+	let {
+		locker_num = '12',
+        attempt_date = 1234567,
+        scanner_uin = '1234-4567-8901-2345',
+	} = $props();
+    const options: Intl.DateTimeFormatOptions = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+    };
+	// svelte-ignore state_referenced_locally
+	let dateOfAttempt = new Date(attempt_date);
+    console.log(dateOfAttempt);
+    dateOfAttempt.toLocaleDateString(undefined, options);
+    let dateString = $derived(new Date(dateOfAttempt).toUTCString());
+    let dateArray = dateString.split(" ");
+    let finalDate = dateArray.slice(0, 4).join(" ");
+</script>
+
+<div
+	class="bg-mlb-white border-mlb-white hover:border-mlb-orange mb-2 flex h-1/12 w-full shrink-0 flex-row rounded-2xl border-2 hover:shadow-sm"
+>
+	<div class="w-1/3 content-center text-center">
+		{finalDate}
+	</div>
+
+	<div class="w-1/3 content-center text-center">
+		{locker_num}
+	</div>
+
+    <div class="w-1/3 content-center text-center">
+		{scanner_uin}
+	</div>
+</div>
--- webapp/src/routes/(protected)/admin/+page.svelte
+++ webapp/src/routes/(protected)/admin/+page.svelte
@@ -1,7 +1,50 @@
 <script>
 	import Navbar from '$lib/components/navbar.svelte';
+	import TableRow from '$lib/components/table_row_unsuccessful_logs.svelte';
+
+	let isNavbarActive = $state(true);
 </script>
 
-<div class="bg-mlb-gray/30 h-screen w-screen">
-	<Navbar />
+<div class="bg-mlb-gray/30 relative flex h-screen w-screen">
+	<Navbar bind:active={isNavbarActive}/>
+
+	<div class="z-0 flex flex-col py-12 pr-12 pl-16 {isNavbarActive ? 'w-4/5' : 'w-full'}">
+		<p class="place-content-center text-3xl font-bold pb-3"> Dashboard </p>
+
+		<div class="flex w-full gap-x-5">
+			<div class="w-1/2">
+				<p class="text-xl font-bold pb-3"> Overall Stats </p>
+
+				<div class="flex flex-row gap-x-5">
+					<div class="p-2 w-1/2 bg-green-100 rounded-lg">
+						<p class="font-medium"> Available Lockers </p>
+						<p> 4 </p>
+					</div>
+
+					<div class="p-2 w-1/2 bg-red-100 rounded-lg">
+						<p class="font-medium"> Overdue Parcels </p>
+						<p> 5 </p>
+					</div>
+				</div>
+			</div>
+			
+			<div class="w-1/2">
+				<p class="text-xl font-bold pb-3"> Security Issues </p>
+
+				<div class="bg-mlb-white rounded-lg">
+					<!-- TABLE header -->
+					<div class="p-2 flex flex-row w-full text-center font-medium">
+						<p class="w-1/3"> Timestamp </p>
+						<p class="w-1/3"> Locker Number </p>
+						<p class="w-1/3"> UIN </p>
+					</div>
+
+					<!-- Logs -->
+					<div class="p-2 flex w-full text-center">
+						<TableRow />
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
